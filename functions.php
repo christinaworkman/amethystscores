ef238e26fb6c433e1f753179f6b808270b1ae626
Christina Workman <christinakworkman@gmail.com>
2022-05-02 22:41:20 -0600

    add social menu and style to footer

--- functions.php
+++ functions.php
@@ -50,6 +50,7 @@ function amethystscores_setup() {
 	register_nav_menus(
 		array(
 			'menu-1' => esc_html__( 'Primary', 'amethystscores' ),
+			'social-menu' => esc_html__( 'Social Media Links', 'amethystscores' ),
 		)
 	);
 
